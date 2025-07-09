324aaea7fc64e78d7d0101cd1b551617b7f9fc3d
Shehzadi Alishba Khan <142626257+Chashmishlish@users.noreply.github.com>
2025-07-09 17:40:02 +0000

    ye file hai ye push he nahi horahi hai commit krde hai 30mint se load horhi
    commit kerta wa  commit define kerna hota ha means initail commit  arrey bhae ya to ajjeb aap na github se vs code apna chrome per he opn kera ha
    jee mene code space k through kra hai

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 1234
+let accountEmail = "alishba@gmail.com"
+var accountPassword = "09876"
+accountCity = "Islamabad" 
+let accountState;
+//bina variable k bhi variable ke memory reserve kre ja skte hy
+
+
+// accountId = 2 //not allowed 
+
+accountEmail = "alish@gmail.com"
+accountPassword = "ak1212"
+accountCity = "Lahore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Another comments method in javascript 
+Prefer not to use var
+because of the issue in block scope and functional scope
+*/
+
+
+// terminal execution line: node 01_basics/01_variables
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+//execution ; node 01_basics/02_dataTypes
+"use strict"; //treat all JS code as newer version
+
+// alert (3 + 3) // it throws an error because we are using nodeJs, not browser
+
+// console.log (3+3);  console.log ("Alishba") //not readible 
+
+let name = "Alishba" //string
+let age = 19 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53
+// bigint - prefer to use in areas like stock market, trading where number is in huge quantity
+// string => "" , ''
+// boolean => true/false
+// null => standalone value, special value, empty
+// undefined => a value is not declared or assigned yet
+// symbol => for uniqueness - implementation in react mostly
+
+
+//Object : null
+
+console.log(typeof null); //output: Oobject
+console.log(typeof undefined); //output: undefined
+
