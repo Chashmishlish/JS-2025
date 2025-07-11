dbc32e4bb703d1bf1fe18327b570fbe6b8fa4e1b
Chashmishlish <salishkhannn@gmail.com>
2025-07-11 16:58:58 +0500

    var check

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -21,10 +21,8 @@ Prefer not to use var
 because of the issue in block scope and functional scope
 */
 
-<<<<<<< HEAD
- 
- // terminal execution line: node 01_basics/01_variables
-=======
+/// HEAD
+
 //Redirecting in the same scope - using var
  var p = "Ono" 
  var p = "Uno" //No error! because redeclaration alloweded
@@ -46,4 +44,10 @@ fruit = "lime" //Typerror!
 
  
 // terminal execution line: node 01_basics/01_variables
->>>>>>> c1e6ad72176f5e7f31e5b9e9c4bd6253e3b54002
+/// c1e6ad72176f5e7f31e5b9e9c4bd6253e3b54002
+
+// yahan se nahi krskte?? VS se github p?
+// vs se he to kera ha terminal per code line likhni perte hain to aap yaha se direct push ker diya karen ab error nh dega is folder me 
+// okays
+// means source control me aakr, uske baad + sign choose krun, msg box me kuc bhi message likhun then commit krke uske baad push it right?jeee jo box me likhta ha wo hota ha ka kiya changes  ya kiya kaam ker ka push kara ha wo git hub per show hota ha 
+// okay
