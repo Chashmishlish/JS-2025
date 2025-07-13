8f9a2baf5ee5ba9c1bbfb3593cf1dca3e795ddb5
Chashmishlish <salishkhannn@gmail.com>
2025-07-13 23:21:28 +0500

    Testing

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,17 +1,7 @@
 console.log("lish");
 
-let x;
-x = 10;
-xy = 100;
-yy= 1000;
-
-// console.log(x);
-// console.log(xy);
-console.log(`The price of bag is $${xy}`);
-console.log(`The price of pen is $${x}`);
-console.log(`The price of course is $${yy}`)
-//
-
 //commit cammad lines
 // git config --global user.name "Chashmishlish"
 // git config --global user.email "salishkhannn@gmail.com"
+
+
