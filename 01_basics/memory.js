033335bf4364476f8eed11c7fa6faa85dfd7ed8a
Chashmishlish <salishkhannn@gmail.com>
2025-07-13 18:46:54 +0500

    Memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,19 @@
+let myYoutubename = "withlovelishba.com";
+
+let anothername = myYoutubename
+anothername = "Chashmishlish"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+//object
+let userOne = {
+    email : "user@google.com",
+    nayapay : "user@nayapay"
+}
+
+let userTwo = userOne
+userTwo.email = "khanza@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
