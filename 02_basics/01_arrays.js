fa29afe9f4c610b585377990f5e189616444c85b
Chashmishlish <salishkhannn@gmail.com>
2025-07-15 22:37:33 +0500

    Arrays!

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+const myArray = [0, 1, 2, 3, 4 ,5]
+
+const myTeachers = ["Miss Afsheen", "Miss Tarannum", "Miss Noor", "Sir Rashid", "Sir Adnan"]
+const subjects = ["Science", "English", "Urdu", "Grammar", "Maths"];
+const institute = subjects.concat(myTeachers); //one way of concatination
+// const institute = [...subjects, ...myTeachers]; //another way of concatination
+console.log(institute);
+
+
+
+
