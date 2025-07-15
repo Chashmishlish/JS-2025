9c6cf58a2c957cb59186bfc45d35b275159a8f5f
Chashmishlish <salishkhannn@gmail.com>
2025-07-15 23:04:44 +0500

    Arrays Methods!

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,14 +6,31 @@ const subjects = ["Science", "English", "Urdu", "Grammar", "Maths"];
 // const institute = [...subjects, ...myTeachers]; //another way of concatination
 // console.log(institute);
 
-const institute = [
-  { Subject: "Science", Teacher: "Miss Afsheen" },
-  { Subject: "English", Teacher: "Miss Tarannum" },
-  { Subject: "Urdu", Teacher: "Miss Noor" },
-  { Subject: "Grammar", Teacher: "Sir Rashid" },
-  { Subject: "Maths", Teacher: "Sir Adnan" },
-]; //an array of objects:
-console.table(institute); 
+// const institute = [
+//   { Subject: "Science", Teacher: "Miss Afsheen" },
+//   { Subject: "English", Teacher: "Miss Tarannum" },
+//   { Subject: "Urdu", Teacher: "Miss Noor" },
+//   { Subject: "Grammar", Teacher: "Sir Rashid" },
+//   { Subject: "Maths", Teacher: "Sir Adnan" },
+// ]; //an array of objects:
+// console.table(institute); 
+
+// Array Methods
+const bodyParts = ["foot", "hands", "eyes", "mouth"]
+bodyParts.push("nose")
+bodyParts.push("head") //
+bodyParts.pop() //removed last element
+bodyParts.unshift("nails") //added at start
+bodyParts.shift( ) //remove 1st element
+console.log(bodyParts.includes("eyes")); //to check if something is included in array or not
+console.log(bodyParts.indexOf(3)); //-1
+console.log(bodyParts.indexOf("nose")); //4
+
+const newArray = bodyParts.join()
+console.log(newArray);  // ...,..,..
+console.log(bodyParts); // [..,...,..]
+
+
 
 
 
