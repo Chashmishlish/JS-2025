6a1c08bbd19949c8d8624765fd7e7fd7ab4ea662
Chashmishlish <salishkhannn@gmail.com>
2025-07-15 22:38:35 +0500

    an array of an object!

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,9 +2,19 @@ const myArray = [0, 1, 2, 3, 4 ,5]
 
 const myTeachers = ["Miss Afsheen", "Miss Tarannum", "Miss Noor", "Sir Rashid", "Sir Adnan"]
 const subjects = ["Science", "English", "Urdu", "Grammar", "Maths"];
-const institute = subjects.concat(myTeachers); //one way of concatination
+// const institute = subjects.concat(myTeachers); //one way of concatination
 // const institute = [...subjects, ...myTeachers]; //another way of concatination
-console.log(institute);
+// console.log(institute);
+
+const institute = [
+  { Subject: "Science", Teacher: "Miss Afsheen" },
+  { Subject: "English", Teacher: "Miss Tarannum" },
+  { Subject: "Urdu", Teacher: "Miss Noor" },
+  { Subject: "Grammar", Teacher: "Sir Rashid" },
+  { Subject: "Maths", Teacher: "Sir Adnan" },
+]; //an array of objects:
+console.table(institute); 
+
 
 
 
