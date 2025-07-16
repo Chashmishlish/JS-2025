215a5767c51fa36ddc693ed7cc90868f1251c002
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 02:46:03 +0500

    mOre abt Objects!

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -11,6 +11,7 @@ const JsUser = {
     last_name: "Khan",
     "full name": "Alishba Khan",
     mySym: "mykey1", //aese access nai hota
+    [mySym] : "mykey1", //aese hojayega with correct data type 
     location: "Karachi",
     age: 19,
     Email: "alishbakhan@koogle.com",
@@ -22,10 +23,20 @@ console.log(JsUser.Email) //not much good method
 console.log(JsUser["Email"]) //use this method
 console.log(JsUser["full name"]) //we can access full name by using [""] this method
 // console.log(JsUser."full name") //we can't access full name by using dot
-console.log(JsUser.mySym); //mykey1 is the output but in actual it's no used as symbol
 
-console.log(typeof JsUser.mySym); //string ; this is a problem
-console.log(typeof mykey1); //undefined 
+// console.log(JsUser.mySym); //mykey1 is the output but in actual it's no used as symbol
+console.log(JsUser[mySym]); // "mykey1" 
+
+console.log(typeof JsUser[mySym]);   // string (value type)
+console.log(typeof mySym);          // symbol   (key type)
 
 // agar hume mysym: "mykey1" ko symbol ke trhan use krna hai tow object me hume.....
 // [mySym]: "mykey1"; yahan [] square braces lagane parenge
+
+//how to change the value of object
+JsUser.Email = "khanzalish@chatgpt.com"
+
+Object.freeze(JsUser) //changes won't propogate by using this command 
+JsUser.Email = "chashmish@microsoft.com" //ye execute ya impose nai hoga
+
+console.log(JsUser); 
