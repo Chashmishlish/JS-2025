a0fb9b5ba6b82992ac5c29856a82b09a878eeab4
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 02:15:17 +0500

    Using Symbol in Objs!

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,6 +3,7 @@
 
 //object literals 
 
+const mySym = Symbol("key1") //symbol =>datatype , interview concept
 
 
 const JsUser = {
@@ -21,4 +22,10 @@ console.log(JsUser.Email) //not much good method
 console.log(JsUser["Email"]) //use this method
 console.log(JsUser["full name"]) //we can access full name by using [""] this method
 // console.log(JsUser."full name") //we can't access full name by using dot
+console.log(JsUser.mySym); //mykey1 is the output but in actual it's no used as symbol
 
+console.log(typeof JsUser.mySym); //string ; this is a problem
+console.log(typeof mykey1); //undefined 
+
+// agar hume mysym: "mykey1" ko symbol ke trhan use krna hai tow object me hume.....
+// [mySym]: "mykey1"; yahan [] square braces lagane parenge
