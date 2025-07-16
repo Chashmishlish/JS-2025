922aaf548e197f7e5fc9ee4095222bff54493019
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 01:59:09 +0500

    Getting familiar with Objs!

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,24 @@
 //singleton
-Object.create //constructor method
+// Object.create //constructor method
 
 //object literals 
-const JsUser = {}
+
+
+
+const JsUser = {
+    First_name: "Alishba",
+    last_name: "Khan",
+    "full name": "Alishba Khan",
+    mySym: "mykey1", //aese access nai hota
+    location: "Karachi",
+    age: 19,
+    Email: "alishbakhan@koogle.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.Email) //not much good method
+console.log(JsUser["Email"]) //use this method
+console.log(JsUser["full name"]) //we can access full name by using [""] this method
+// console.log(JsUser."full name") //we can't access full name by using dot
+
