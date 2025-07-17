44d8a8b8c10f25f88e4ea08a8b8357852ec7b175
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 23:29:29 +0500

    this with function!

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -24,3 +24,20 @@ console.log(this); //{} => qk hum node environment me hain qk abhi global scope 
 // isi example ko dekhen web browser k console per iska output -window ayega
 // browser me jb bhi engine run krke ga tow wahan global object window object hai
 
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+function chai (){
+    console.log(this) 
+}
+chai() // bht saari values ajaenge
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//aese function me aakr this use nai kr pa rahe hain isliye undefined aya hai
+// ye context object me he kaam krrha hai
+function chai (){
+    let username = "Palishba"
+    console.log(this.username) //undefined
+}
+chai() 
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
