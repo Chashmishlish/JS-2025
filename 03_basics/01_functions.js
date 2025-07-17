fbe442b747fe41d99ad9c9acbd1295972ff619e3
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 17:11:14 +0500

    Function's Basics!

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+// jo bhi code likha hai, 10line ka 20 line ka usko ek package me band krdea
+// wo package aap jitni baar jahan chahy utha kai uski copies le ja skte hain
+
+
+//syntax //function ke definition hai ye
+function sayMyName(){
+   console.log("A");
+   console.log("L");
+   console.log("I");
+   console.log("S");
+   console.log("H");
+} 
+
+//for calling function 
+sayMyName ()
+// () => execute  , sayMyName =>Reference
