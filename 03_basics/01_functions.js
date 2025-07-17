96220559aa3f45b89ad595ce3b83c20edf7ac168
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 17:47:49 +0500

    Function's Basics!

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,5 +12,33 @@ function sayMyName(){
 } 
 
 //for calling function 
-sayMyName ()
-// () => execute  , sayMyName =>Reference
+// sayMyName () // () => execute  , sayMyName =>Reference
+
+//for adding two numbers
+function addTwoNumbers(number1 , number2){
+    // number1 + number2  //add hogaya but print nai tow console.log krenge
+    console.log(number1 + number2);   
+}
+// addTwoNumbers() //NaN, because num1, num2 me pta he nh kea value the, koi argument () he pass nhi kea 
+addTwoNumbers(3, 14)      // 17
+addTwoNumbers(3, "14")   // 314
+addTwoNumbers(3, "A")   // 3A
+addTwoNumbers(3, null) // 3
+
+
+//(number1 , number2) => these are the parameters, parameter means input
+//addTwoNumbers(3, null) => function calling k time per jo value pass hote hai wo argument hota h
+
+// Concept of storing the function call in the variable 
+function toadd (num1, num2) {
+    console.log(num1 + num2);
+}
+
+toadd(3, 9) //12
+const result = toadd(3, 3) //ye hogaya variable me store //6
+console.log("Result:", result); //undefined
+
+//result k ander value undefined kese gayi? 
+// yei interesting concept hai k, function se humne kya wapis bejha
+
+
