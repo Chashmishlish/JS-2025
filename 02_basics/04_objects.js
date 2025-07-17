fca9f68d9431b3e19d4c87068c573e03da2ed048
Chashmishlish <salishkhannn@gmail.com>
2025-07-17 14:53:08 +0500

    De' Structuring in Objects!

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,5 +24,26 @@ const navbar = ({company}) => {
 
 navbar({ company: "Chashmish" }); //Output: Welcome to Chashmish navbar!
 
+//APIS => jab bhi apne kaam kisi or k sir per dalna ho tw wo API
+//Backend se kch values aati hain un values ko kese likhte hain that's API
+// XML structure me aate then phele , ab JSON me aati hain
 
+/* JSON
+{
+    name: "Alishba",
+    coursename: "Biology",
+    price: "free"
+}
+    */
 
+//uRL : https://api.github.com/users/Chashmishlish
+
+//API in Array
+[
+    {},
+    {},
+    {},
+]
+
+// API : https://randomuser.me/
+//to understand JSON formata : json formatter => https://jsonformatter.org/
