8f9c121792777f6ff0307b1e4113efed38048512
Chashmishlish <salishkhannn@gmail.com>
2025-07-18 18:01:02 +0500

    Basics Of IIFE in JS!

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expression (IIFE)
+//>> Ek aisa function jo likhne ke baad foran execute ho jata hai bina usko manually call kiye.
+/*
+   Jese he function likha usko foran he execute kerwana hai
+   Ab isko karwanay k kaafi ways ho sakte hain, Q hume aesa function chaye 
+   jo  immediately execyte hojaye?! tw kahen baar we will do these kind of things\
+   especially kahen bar hum ek file likhte hain jisme sirf database connection hai
+   or hum chahte hain hmare application jese he start ho wese he us file k ander
+   humara database ka connection start hojaye...
+   Sirf itna he nahi kahen bar kiya hota hai k hum GLOBAL SCOPE jo hai, kahen bar 
+   us scope k ander kuch variables declare hote hain or kahen bar hoskta hai hum us 
+   function k ander kuch likhre hain or hum nai chahte k us function k ander wo 
+   global variables problem kren qk wo tow available hain 
+   (bachay tow ja kar baro se mang skte hain) tow usi trhan se jo bhi hum function me
+   ddeclare krte hain wo kahen bar global scope se pollute hojate hain
+   tow ye 2 reasons hote hain...global scope se pollution hume nahi chahye
+   tow hum alag ek scope bana lete hain function k ander takay pollution ko avoid krsken
+   Or 2nd reason ye k hum chahte hain immediately execute hojaye wahan pe...
+*/
+
