67fcffe83e2953d69001b76272481dc5ca88c22c
Chashmishlish <salishkhannn@gmail.com>
2025-07-18 18:30:57 +0500

    Explored about IIFE!

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -2,6 +2,44 @@
 // Ek aisa function jo likhne ke baad foran execute ho jata hai bina usko manually call kiye.
 
 function company (){
-    console.log('DB CONNECTED');
+    console.log('DB CONNECT');
 }
-company() //DB CONNECTED
+company(); //DB CONNECT
+
+
+(function institute (){    //this function is named IIFE 
+    console.log('DB CONNECTED');
+
+}) (); //DB CONNECTED
+
+//(function...(){....) => ye 1st paranthesis tw functionn ke definition ko enclosed krne k liye hain
+// }) () => ye 2nd paranthesis iski execution call hai jese hum comany () per laga rhy the tow humne function ko wrap krdea
+// ; semicolon is must for proper execution 
+
+console.log(" ");
+console.log(">>>>>ARROW FUNCTION>>>>");
+
+// ( () => {
+//     console.log(`DB CONNECTED TOO`);
+
+// }) () //type error >> because humne semicolon nahi lagaya (); iske end me
+
+
+(function software () {
+    console.log(`DB CONNECTED TOO`);
+
+}) (); //DB CONNECTED TOO
+
+
+( () => {
+    console.log(`YOUR DB IS CONNECTED `);
+
+}) ();  //YOUR DB IS CONNECTED
+//to end a code we need to put ; this at the end wrna next wala execute nai hoga func
+
+( (name) => {         //this is a simple/unnamed IIFE
+    console.log(`${name}, YOUR DB IS CONNECTED `);
+
+}) ("Chashmish"); //Chashmish, YOUR DB IS CONNECTED 
+
+
