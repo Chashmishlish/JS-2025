8444495eb8a1013a12b3294629984a1aaf5841af
Chashmishlish <salishkhannn@gmail.com>
2025-07-20 20:37:47 +0500

    Nullish Operator!

--- 04_Control_flow/truthy.js
+++ 04_Control_flow/truthy.js
@@ -25,7 +25,24 @@ if ( Object.keys(emptyObj).length === 0 ){
     console.log("Object is empty");
 }
 
-     
+/* Nullish Coalescing Operator (??) : null, undefined
+ kahen bar jab hum database se call krenge ya firebase, appright ka use krenge
+ tow hume directly response nai milta hai, hume do values milte hain
+ tow chances hain hoskta hai humare pass null response ajaye ya hoskta hai response aye he na(undefined)
+ tow us case me apka pura ka pura code structure compromised hoskta hai tow un cases k liye ye banaya hy
+ k values ko apko treat krna hai k acha null ho tw usme null assign krlo takay us hisab se case ko adjust 
+ krlun mai or agar undefined hai tw thk hai wrna tw koi value assign krdo
+ us special case me jo hum Database me agay ja kr prhen ge
+*/
+let val1;
+
+val1 = 5 ?? 10
+val2 = null ?? 10
+val1 = undefined ?? 15
+
+
+console.log(val1);
+
 
     
 
