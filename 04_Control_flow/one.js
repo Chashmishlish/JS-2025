3fa46911570f71564a92aec56b1b400d11519680
Chashmishlish <salishkhannn@gmail.com>
2025-07-20 18:41:14 +0500

    IF statement!

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -1,11 +1,15 @@
 /* if statement 
 if (condition){
-
+    ...................
 }
 if condition is true then the code inside { } will be exeuted
 if condition is false then the code inside {} won't be executed
 
+Operators of comparison : < , > , <= (2<=2) , >= , ==
+= ; operator assignment , == ; to check whether it is equal, != ; not equal
+eg: (3 != 2) = true
 */
+
 
 const isUserloggedIn = true
 const temperature = 41
@@ -31,9 +35,17 @@ if ( temperature < 40 ) {
 }
 console.log("executed"); //executed ; 
 
+if (3 != 4){
+    console.log("3 is not less than 4"); //3 is not less than 4
+}else{
+    console.log("3 is less than 4");
+}
 
-/*
-Operators of comparison : < , > , <= (2<=2) , >= , ==
-= ; operator assignment , == ; to check whether it is equal, != ; not equal
-eg: (3 != 2) = true
-*/
+const score = 300
+if (score > 100 ) {
+    let  power = "fly"
+    console.log(`User power: ${power}`) //User power: fly
+}
+console.log(`User power: ${power}`) // Ref error: power is not defined
+// this is because of scope
+
