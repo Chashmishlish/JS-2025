9eedbc7cf091e4176bd442f665f145268480426b
Chashmishlish <salishkhannn@gmail.com>
2025-07-21 22:26:09 +0500

    fOrEach lOOps!

---
+++ 05_loops/for_each.js
@@ -0,0 +1,10 @@
+//useful for arrays
+
+const coding = ["Js", "ruby", "Cpp", "java", "swift", "CSS", "Html", "Python"]
+
+// coding.forEach( function name ()  {} ) //since we have to include a call back func in forEach so we will remove the function name
+coding.forEach( function (item)  {
+    console.log(item);
+});
+
+
