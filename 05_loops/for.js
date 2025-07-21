91fda55960a3974dfdee63d586a12716965a256d
Chashmishlish <salishkhannn@gmail.com>
2025-07-21 17:14:37 +0500

    Basics of for-loOp!

---
+++ 05_loops/for.js
@@ -0,0 +1,34 @@
+
+
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];  
+}
+    This loop is used to iterate over all elements of an array. 
+   index is the name of variable, 
+   then we have condition with the property length
+   index ++ horaha hai. 
+   jese he condition check hojate hai or agar true hai wese he loop k ander ajate hain 
+   const element = array [index]
+   isko aese chalaya tow error ajaye ga k array is not defined
+
+   🔁 for (let index = 0; index < array.length; index++)
+This is the loop structure:
+let index = 0: Start with index 0 (first element of the array).
+index < array.length: Keep looping as long as index is less than the total number of elements in the array.
+index++: After each loop, increase the index by 1 (go to the next element).
+    📦 const element = array[index];
+This line gets the element at the current index from the array and saves it in the variable element.
+You can now use element inside the loop body to do something with each item.
+
+ctrl d
+*/
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 6) {
+        console.log(" 6 is the best number ") //phele ye print hoga 
+    }
+    console.log(element); 
+}
+
+// console.log(element);
