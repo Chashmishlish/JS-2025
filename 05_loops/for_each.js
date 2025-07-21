f7f3d41765b4ae9146037a5cf5c530ac8e41f613
Chashmishlish <salishkhannn@gmail.com>
2025-07-21 22:37:36 +0500

    For'Each Callback & inline Function!

--- 05_loops/for_each.js
+++ 05_loops/for_each.js
@@ -4,11 +4,25 @@
 const coding = ["Js", "ruby", "Cpp", "java", "swift", "CSS", "Html", "Python"]
 
 coding.forEach( function (item)  {
-    console.log(item);
+    // console.log(item);
 });
 
 //arrow function -- arrow ka name store krne ke need nai hai qk ye callback function h
 coding.forEach( (val) =>  {
-    console.log(val);
-    
+    // console.log(val);
 } )
+
+
+//ForEach with Callback Function: printMe function ko seedha forEach ko de diya gaya hai. Ye reusable function hai.
+function printMe(item) {
+    console.log(item);
+}
+coding.forEach(printMe)
+
+//ForEach with Inline Arrow Function: forEach ke andar direct ek function likh diya gaya hai jo har baar call hoga.
+const framework = ["React" , "Angular", "Laravel"]
+framework.forEach( (item, index ,arr ) => {
+    console.log(item, index, arr);
+})
+
+
