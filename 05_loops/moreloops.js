9e7ed508a9e22856363ebd606400ed90f37279a1
Chashmishlish <salishkhannn@gmail.com>
2025-07-21 21:33:02 +0500

    fOr Of lOop!

---
+++ 05_loops/moreloops.js
@@ -0,0 +1,16 @@
+/* For of => array specific loop
+
+[ "" , "" , "" ]
+[ {} , {} , {} ]
+
+for (const iteration of object) {  
+    //iteratin => variable name, object => kis object per loop lagana chah rahe hain
+}
+    */
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
