8567162d86df0780c2cf00d1a3e25da08db84fcc
Chashmishlish <salishkhannn@gmail.com>
2025-07-21 22:28:45 +0500

    fOrEach with arrow function!

--- 05_loops/for_each.js
+++ 05_loops/for_each.js
@@ -1,10 +1,14 @@
 //useful for arrays
+// coding.forEach( function name ()  {} ) //since we have to include a call back func in forEach so we will remove the function name
 
 const coding = ["Js", "ruby", "Cpp", "java", "swift", "CSS", "Html", "Python"]
 
-// coding.forEach( function name ()  {} ) //since we have to include a call back func in forEach so we will remove the function name
 coding.forEach( function (item)  {
     console.log(item);
 });
 
-
+//arrow function -- arrow ka name store krne ke need nai hai qk ye callback function h
+coding.forEach( (val) =>  {
+    console.log(val);
+    
+} )
