6c479eed06e4dfe169d2e5bf4630e1ffa7b2b078
Chashmishlish <salishkhannn@gmail.com>
2025-07-23 22:32:41 +0500

    Basic Understanding of "accumulator"

---
+++ 05_loops/reduce.js
@@ -0,0 +1,6 @@
+// accumulate : gather together or acquire an increasing number or quantity of.
+// accumulator : a register, or a memory location used to store the intermediate results of arithmetic and logical operations.
+// In JavaScript, an accumulator is just a variable that keeps a running total or combined result during a loop or operation.
+// Think of it like a box where we keep adding things as we loop. 📦
+
+
