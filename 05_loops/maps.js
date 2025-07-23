9a72ffa20aef2be4788882b01cac566d7c44bc7c
Chashmishlish <salishkhannn@gmail.com>
2025-07-23 22:16:15 +0500

    Chaining in maps!!

--- 05_loops/maps.js
+++ 05_loops/maps.js
@@ -17,3 +17,19 @@ console.log("The numbers are multiplied by 2:", newNumb);
  //JavaScript automatically converts the array to a string when it’s inside ${...}.
 
 
+//we can also do it using for each
+console.log(">>>>  using forEach  >>>>");
+
+const newN = myNumbers.forEach( (num) => {
+    return num + 10
+})
+console.log(newN);  //undefined
+
+console.log(">>>>  Chaining  >>>>");
+
+const newNums = myNumbers
+                .map( (num) => num * 10 -  3 )     //it result pass into 2nd .map()
+                .map( (num) => num + 1  )         // [8, 18, 28, 38, 48, 58, 68, 78, 88, 98]
+                .filter( (num) => num >= 48)  // [ 48, 58, 68, 78, 88, 98 ]
+
+console.log(newNums);
