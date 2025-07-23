80ae3ec64b346380a03ce69ff6b36f0bb3814fbd
Chashmishlish <salishkhannn@gmail.com>
2025-07-23 20:58:15 +0500

    foreach doesn't obey return!

--- 05_loops/more.js
+++ 05_loops/more.js
@@ -1,5 +1,29 @@
 const coding = ["JS" , "PY" , "CPP" , "HTML" , "CSS"]
 
-coding.forEach( (item) => {
+const values = coding.forEach( (item) => {
     console.log(item);
 })
+
+console.log(values)  //undefined return kuch bhi nahi hua
+// manaually return krwana parega
+
+const code = ["JS" , "PY" , "CPP" , "HTML" , "CSS"]
+
+const value = code.forEach( (item) => {
+    console.log(item);
+    return item
+})
+
+console.log(value)   //undefined
+// again we saw the value undefined even after using the return
+// agar hum     console.log(item); comment krden tb bhi undefined he ayega
+
+console.log("It means FOR EACH () doesn't return any value ");
+
+
+
+
+
+
+
+
