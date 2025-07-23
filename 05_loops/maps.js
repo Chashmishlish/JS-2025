29e5f975c3946fb94dcaefa0abb8fdb99f02b456
Chashmishlish <salishkhannn@gmail.com>
2025-07-23 22:04:59 +0500

    Example of maps!

--- 05_loops/maps.js
+++ 05_loops/maps.js
@@ -3,5 +3,17 @@
 const myNumbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 const newNumbers = myNumbers.map( (num) => num + 10 )
-console.log(newNumbers);
+console.log("The numbers are added by 10:", newNumbers);
+
+const newNumber = myNumbers.map( (num) => { return num + 20 })
+console.log("The numbers are added by 20:", newNumber);
+ 
+const newNumb = myNumbers.map( (num) => { 
+    return num * 2
+    })
+
+console.log("The numbers are multiplied by 2:", newNumb);
+// console.log(` The numbers are multiplied by 2:  ${newNumb}`);
+ //JavaScript automatically converts the array to a string when it’s inside ${...}.
+
 
