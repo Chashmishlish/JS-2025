b5cc773e908541e3c3a508c54e11302e6f952aa9
Chashmishlish <salishkhannn@gmail.com>
2025-07-24 20:44:37 +0500

    Array testing!

--- 05_loops/arraytest.js
+++ 05_loops/arraytest.js
@@ -28,3 +28,46 @@ function flattenArray(nested) {
 }
 console.log(flattenArray(nested)); // [1, 2, 3, 4, 5, 6]
 
+let Movienames = [ "Interstellar" , "Inception", "Deadpool", "Titanic", "Joker"];
+
+const addMN = Movienames.splice(1, 0, "Hey Baby");
+const remMN = Movienames.splice(1, 4, "Jurrasic world");
+const repMN = Movienames.splice(1, 4);
+
+console.log(addMN);
+console.log(remMN);  
+console.log(repMN);  
+console.log(Movienames);
+
+let fruits = [ "apple", "banana",  "mango", "banana", "Orange" , "Banana"];
+
+let rem = fruits.indexOf("banana", fruits.indexOf("banana") + 1 , fruits.indexOf("Banana")); 
+fruits.splice(rem, 1);
+console.log(fruits); 
+
+
+let names = ["Ali", "Hamaza", "Zara"];
+console.log(names);
+let reversed = names.reverse();
+console.log(names);
+console.log(reversed);
+
+let num = [3, 5, 7, 10, ];
+function evenNos (num) {
+    return num.find( (i) => i % 2 == 0 );
+}
+console.log(evenNos(num));
+
+let Myarray = [1, 2, 3, 4, 5];
+let [a, b, ...rest] = Myarray;
+console.log(a);    // 1
+console.log(b);    // 2
+console.log(rest); // [3, 4, 5]
+
+// let arr = [1, 2, 3, 4, 5];
+// let a = arr[0]
+// let b = arr[1]
+// let rest = arr.slice(2)
+// console.log(a);    
+// console.log(b);    
+// console.log(rest);
