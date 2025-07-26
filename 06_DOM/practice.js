110be9ede06012ba90c511fc576b35a998562673
Chashmishlish <salishkhannn@gmail.com>
2025-07-26 19:38:10 +0500

    prompt!

---
+++ 06_DOM/practice.js
@@ -0,0 +1,8 @@
+console.log(`Hello`);
+console.log(`Alishba`);
+
+window.alert(`I am an alert!`);
+window.alert(`I am the best!`);
+
+document.getElementById("heads").textContent = `Brendan Eich`;
+document.getElementById("myP").textContent = `The Founder of JavaScript`;
