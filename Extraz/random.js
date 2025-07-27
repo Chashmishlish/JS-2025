8ecc9989943f2c162592ff74fd239208897da44c
Chashmishlish <salishkhannn@gmail.com>
2025-07-27 17:19:54 +0500

    max & min

--- Extraz/random.js
+++ Extraz/random.js
@@ -11,7 +11,11 @@ console.log(RandomNum);
 const min = 50;
 const max = 100; 
 
-let ranNum = Math.floor(Math.random() * max ) + min;
+let ranNum = Math.floor(Math.random() * (max  - min)) + min;
+let ranNumm = Math.floor(Math.random() * max) + min;  //146, 59, 122
 
 console.log(ranNum);
+console.log(ranNumm);
+
+// (max  - min) ; number should be b/w 50 and  100 
 
