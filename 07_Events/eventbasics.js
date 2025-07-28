a7e270ad45968bdecbc92f3f73eb41d365add1cc
Chashmishlish <salishkhannn@gmail.com>
2025-07-28 18:44:49 +0500

    generate random color logic!

---
+++ 07_Events/eventbasics.js
@@ -0,0 +1,11 @@
+// Generate a random color 
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0 ; i < 6 ; i++){
+        color += hex[Math.floor(Math.random() * 16)]  //+= ; bcz continuously add krni hain values
+    } 
+    return color; 
+}
+console.log(randomColor());
