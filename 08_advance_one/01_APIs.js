90af9c5e014d1074bb2a98d55e120f961f6292b9
Chashmishlish <salishkhannn@gmail.com>
2025-07-28 22:45:18 +0500

    XML Http Request!

---
+++ 08_advance_one/01_APIs.js
@@ -0,0 +1,10 @@
+// API is a talking language between two systems. Two systems can be frontend and backend. It can be both our backend-our frontend. and could be someone else's backend and our fronend and vice versa. Maybe, two systems want to talk to each other. One is written in JS another is written in Java. Or one is written in Ruby, one is written in PHP. So, the medium which is used to communicate between two is called an API. 
+
+// Famous API: 
+// GitHub 
+// randomuser.me
+
+// Json formatter: It is used to read API data or to understand API format. 
+//                 https://jsonformatter.org/
+
+
