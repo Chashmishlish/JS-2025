b3cd1d4ac8423fc298197e37d1449ae7f03b626c
Chashmishlish <salishkhannn@gmail.com>
2025-07-29 15:54:37 +0500

    Understanding promises!

---
+++ 08_advance_one/02_Promises.js
@@ -0,0 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+// Most Imp : The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+// jo bhi task apko diya gaya hai wo queue me lagh gaya hai magar laghte he complete nai hoga
+
+// 3 states of promises: pending, fulfilled, rejected
+
