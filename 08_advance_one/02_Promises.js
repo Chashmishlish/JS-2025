660b3f96385f89babd760295d2e58bc5933108a6
Chashmishlish <salishkhannn@gmail.com>
2025-07-29 17:51:21 +0500

    Promise (Error based checking)

--- 08_advance_one/02_Promises.js
+++ 08_advance_one/02_Promises.js
@@ -8,43 +8,67 @@
 // libraries : Q and blue bird but ab node js ne direct support dedea hai (native promises) 
 
 
-// How promises are made
-const promiseOne = new Promise(function(resolve, reject){
-    // Do an async task 
-    // DB calls, cryptography, network
-    setTimeout(function() {
-        console.log('Async task is complete');
-        resolve()
-    }, 1000);
-})
+// // How promises are made
+// const promiseOne = new Promise(function(resolve, reject){
+//     // Do an async task 
+//     // DB calls, cryptography, network
+//     setTimeout(function() {
+//         console.log('Async task is complete');
+//         resolve()
+//     }, 1000);
+// })
 
-// promise ko humne ek variable me store kea, create krlea ab consume krna hai
-//.then ka straight connection hai resolve k saath .then k ander ek callback milta hai means function
+// // promise ko humne ek variable me store kea, create krlea ab consume krna hai
+// //.then ka straight connection hai resolve k saath .then k ander ek callback milta hai means function
 
-promiseOne.then(function(){
-    console.log("Promise consumed"); //ye tow print he nahi hua
-})
+// promiseOne.then(function(){
+//     console.log("Promise consumed"); //ye tow print he nahi hua
+// })
 
-// ab connect krenge in dono ko by resolve ()
-// hum yehi kaam variable me store kre bina bhi krskte hain (see below)
+// // ab connect krenge in dono ko by resolve ()
+// // hum yehi kaam variable me store kre bina bhi krskte hain (see below)
 
-new Promise(function(resolve, reject){
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task 2!");
+//         resolve(); 
+//     }, 3000)
+// }).then(function(){
+//     console.log("Async 2 resolved!");
+// });
+
+
+// // to pass data
+// const promiseThree = new Promise(function (resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "Alishba" , email: "alishba@example.com"})
+//     }, 4000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user); //{ username: 'Alishba', email: 'alishba@example.com' }
+// })
+
+// Error based checking per either true or false
+const promiseFour = new Promise(function (resolve, reject){
     setTimeout(function(){
-        console.log("Async task 2!");
-        resolve(); 
-    }, 3000)
-}).then(function(){
-    console.log("Async 2 resolved!");
-});
-
-
-// to pass data
-const promiseThree = new Promise(function (resolve, reject){
-    setTimeout(function(){
-        resolve({username: "Alishba" , email: "alishba@example.com"})
+        let error = true
+        if (!error) {
+            resolve({username: "Alishba" , password: "123"})
+        }else{
+            reject('ERROR: Something went wrong!')
+        }
     }, 4000)
 })
 
-promiseThree.then(function(user){
-    console.log(user); //{ username: 'Alishba', email: 'alishba@example.com' }
+const username = promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {    //chaning concept
+    console.log(username); 
+}).catch(function(err){
+    console.log(err);
 })
+
+
+
