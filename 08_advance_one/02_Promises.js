5922b3a87b3052702f8de6bd89fb475ea0a35625
Chashmishlish <salishkhannn@gmail.com>
2025-07-31 00:31:17 +0500

    console.log(response);

--- 08_advance_one/02_Promises.js
+++ 08_advance_one/02_Promises.js
@@ -121,8 +121,10 @@ consumePromiseFiveClone()
 async function getAllUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    console.log(data);
+    console.log(response);
+
+    // const data = await response.json();
+    // console.log(data);
   } catch (error) {
     console.log("E:", error);
   }
