db40ea42adc4e67dfd66763b5699fd2de817292b
Chashmishlish <salishkhannn@gmail.com>
2025-07-31 00:33:03 +0500

    syntax for try , catch!

--- 08_advance_one/02_Promises.js
+++ 08_advance_one/02_Promises.js
@@ -121,15 +121,15 @@ consumePromiseFiveClone()
 async function getAllUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(response);
-
-    // const data = await response.json();
-    // console.log(data);
+    // console.log(response);
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
     console.log("E:", error);
   }
 }
-
 getAllUsers();
+
+
 
 
