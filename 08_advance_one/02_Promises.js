d771b631dae9ad11c19d4875622fb1f8fcb84514
Chashmishlish <salishkhannn@gmail.com>
2025-07-30 23:56:45 +0500

    Fetch!

--- 08_advance_one/02_Promises.js
+++ 08_advance_one/02_Promises.js
@@ -116,10 +116,18 @@ async function consumePromiseFiveClone(){
 }
 consumePromiseFiveClone()
 
-//fetch
-async function getAllUsers(){
-   const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
-   const data = response.json()
-   console.log(data);
+// Ek async function define kiya gaya hai jo users fetch karega
+
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E:", error);
+  }
 }
-getAllUsers()
+
+getAllUsers();
+
+
